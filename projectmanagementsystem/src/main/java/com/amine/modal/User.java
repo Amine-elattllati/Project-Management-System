adc1bdc53ef93f3ec9a13b793e4a494539515cb2
Amine-elattllati <126120495+Amine-elattllati@users.noreply.github.com>
2024-05-02 00:55:19 +0100

    CustomeUserDetailsImpl

---
+++ projectmanagementsystem/src/main/java/com/amine/modal/User.java
@@ -0,0 +1,28 @@
+package com.amine.modal;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "project_user")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String fullName;
+    private String email;
+    private String password;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "assignee",cascade = CascadeType.ALL)
+    private List<Issue> assignedIssues = new ArrayList<>();
+
+    private int projectSize;
+}
